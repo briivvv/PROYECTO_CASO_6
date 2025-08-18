3bb5b885d9bc8a8d82c95284bc2c54981e43f80d
jeanproyect <jean.29.12.2006@gmail.com>
2025-08-18 12:44:54 -0400

    jean1

--- carousel.js
+++ carousel.js
@@ -4,7 +4,7 @@ let current = 0;
 function showNextImage() {
   images[current].classList.remove('active');
   current = (current + 1) % images.length;
-  images[current].classList.add('active');
+  images[current].classList.add('active');//listo
 }
 
 images[current].classList.add('active'); // Mostrar la primera imagen
